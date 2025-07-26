d184cf31b1f2961fc5c2848a960233b02758dbf3
ashu-tagore <aswat0thama@gmail.com>
2025-07-26 14:02:42 +0545

    Bridge

--- backend/analytics/admin.py
+++ backend/analytics/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from curriculum.models import Grade, Subject, Topic
+@admin.register(Grade)
+class GradeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'order', 'is_active')
+    search_fields = ('name',)
+    ordering = ('order',)
